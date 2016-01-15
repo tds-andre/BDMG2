74333e0e4444c4f0cf627c1826526d0eff4b30dd
tds-andre <tds.andre@gmail.com>
2016-01-14 22:28:27 -0200

    zero

---
+++ syst/Esparsador/impl/wrk/VS2013Dsktp/Tds.Prjs.Bdmg.Esparsador/FormMain.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Tds.Prjs.Bdmg.Esparsador
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BinCountControl = new System.Windows.Forms.NumericUpDown();
+            this.NumericFieldsControl = new System.Windows.Forms.TextBox();
+            this.FilenameControl = new System.Windows.Forms.TextBox();
+            this.ProcessButton = new System.Windows.Forms.Button();
+            this.SeparatorControl = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.OutputStartIndexControl = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.BinCountControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputStartIndexControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Caminho do arquivo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(473, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Índice dos campos numéricos a serem discretizados (indíce começa em 0, separe-os " +
+    "com vírgulas)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Quantidade de bins";
+            // 
+            // BinCountControl
+            // 
+            this.BinCountControl.DataBindings.Add(new System.Windows.Forms.Binding("Value", global::Tds.Prjs.Bdmg.Esparsador.Properties.Settings.Default, "BinCount", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.BinCountControl.Location = new System.Drawing.Point(15, 103);
+            this.BinCountControl.Name = "BinCountControl";
+            this.BinCountControl.Size = new System.Drawing.Size(96, 20);
+            this.BinCountControl.TabIndex = 9;
+            this.BinCountControl.Value = global::Tds.Prjs.Bdmg.Esparsador.Properties.Settings.Default.BinCount;
+            // 
+            // NumericFieldsControl
+            // 
+            this.NumericFieldsControl.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::Tds.Prjs.Bdmg.Esparsador.Properties.Settings.Default, "NumericFields", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.NumericFieldsControl.Location = new System.Drawing.Point(12, 64);
+            this.NumericFieldsControl.Name = "NumericFieldsControl";
+            this.NumericFieldsControl.Size = new System.Drawing.Size(473, 20);
+            this.NumericFieldsControl.TabIndex = 8;
+            this.NumericFieldsControl.Text = global::Tds.Prjs.Bdmg.Esparsador.Properties.Settings.Default.NumericFields;
+            // 
+            // FilenameControl
+            // 
+            this.FilenameControl.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::Tds.Prjs.Bdmg.Esparsador.Properties.Settings.Default, "Filename", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.FilenameControl.Location = new System.Drawing.Point(12, 25);
+            this.FilenameControl.Name = "FilenameControl";
+            this.FilenameControl.Size = new System.Drawing.Size(473, 20);
+            this.FilenameControl.TabIndex = 1;
+            this.FilenameControl.Text = global::Tds.Prjs.Bdmg.Esparsador.Properties.Settings.Default.Filename;
+            // 
+            // ProcessButton
+            // 
+            this.ProcessButton.Location = new System.Drawing.Point(491, 25);
+            this.ProcessButton.Name = "ProcessButton";
+            this.ProcessButton.Size = new System.Drawing.Size(77, 97);
+            this.ProcessButton.TabIndex = 10;
+            this.ProcessButton.Text = "Processar";
+            this.ProcessButton.UseVisualStyleBackColor = true;
+            this.ProcessButton.Click += new System.EventHandler(this.ProcessButton_Click);
+            // 
+            // SeparatorControl
+            // 
+            this.SeparatorControl.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::Tds.Prjs.Bdmg.Esparsador.Properties.Settings.Default, "Separator", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.SeparatorControl.Location = new System.Drawing.Point(114, 102);
+            this.SeparatorControl.Name = "SeparatorControl";
+            this.SeparatorControl.Size = new System.Drawing.Size(96, 20);
+            this.SeparatorControl.TabIndex = 11;
+            this.SeparatorControl.Text = global::Tds.Prjs.Bdmg.Esparsador.Properties.Settings.Default.Separator;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(117, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Separador";
+            // 
+            // OutputStartIndexControl
+            // 
+            this.OutputStartIndexControl.DataBindings.Add(new System.Windows.Forms.Binding("Value", global::Tds.Prjs.Bdmg.Esparsador.Properties.Settings.Default, "OutputIndex", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.OutputStartIndexControl.Location = new System.Drawing.Point(219, 103);
+            this.OutputStartIndexControl.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            this.OutputStartIndexControl.Name = "OutputStartIndexControl";
+            this.OutputStartIndexControl.Size = new System.Drawing.Size(96, 20);
+            this.OutputStartIndexControl.TabIndex = 13;
+            this.OutputStartIndexControl.Value = global::Tds.Prjs.Bdmg.Esparsador.Properties.Settings.Default.OutputIndex;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(216, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(243, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Índici do primeiro campo de saída (-1 para ignorar)";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 139);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.OutputStartIndexControl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SeparatorControl);
+            this.Controls.Add(this.ProcessButton);
+            this.Controls.Add(this.BinCountControl);
+            this.Controls.Add(this.NumericFieldsControl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FilenameControl);
+            this.Name = "FormMain";
+            this.Text = "Esparsador de Dados";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.BinCountControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputStartIndexControl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FilenameControl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox NumericFieldsControl;
+        private System.Windows.Forms.NumericUpDown BinCountControl;
+        private System.Windows.Forms.Button ProcessButton;
+        private System.Windows.Forms.TextBox SeparatorControl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown OutputStartIndexControl;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ syst/Esparsador/impl/wrk/VS2013Dsktp/Tds.Prjs.Bdmg.Esparsador/FormMain.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tds.Prjs.Bdmg.Esparsador.Properties;
+
+namespace Tds.Prjs.Bdmg.Esparsador
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const char csep = '\t';
+            const String ssep = "\t";
+            StreamReader reader = new StreamReader("C:/_/Projetos/BDMG/syst/Sinais/data/repo/input.csv");
+            var header = reader.ReadLine();
+            var headers = header.Split(csep).ToList();
+            var firstrun = true;
+            List<List<String>> lines = new List<List<String>>();
+            while (!reader.EndOfStream) {
+                var line = reader.ReadLine();
+                var fields = line.Split(csep).ToList();
+                lines.Add(fields);                
+            }
+            reader.Close();
+            
+            var sample = lines[0];
+            List<int> indexes = new List<int>();
+            for(int i = 0; i < sample.Count; i++){
+                Decimal dummy;                
+                if (!Decimal.TryParse(sample[i], out dummy)) {
+                    indexes.Add(i);
+                }
+            }
+
+            List<List<String>> dimensions = new List<List<String>>();
+            foreach (var index in indexes) {
+                dimensions.Add(lines.Select(line => line[index]).Distinct().ToList());
+            }
+
+            StreamWriter writer = new StreamWriter("C:/_/Projetos/BDMG/syst/Sinais/data/repo/input.sparsed.csv");
+            writer.Write(header);
+            foreach (var index in indexes){
+                var ix = indexes.IndexOf(index);
+                foreach (var dim in dimensions[ix]) {
+                    writer.Write(ssep + headers[index] + " - " + dim);
+                }
+            }
+            writer.WriteLine();
+
+            foreach (var line in lines) {
+                writer.Write(String.Join(ssep, line));
+                foreach (var index in indexes)
+                {
+                    var ix = indexes.IndexOf(index);
+                    var val = line[index];
+                    foreach (var dim in dimensions[ix])
+                    {
+                        if (dim == val) {
+                            writer.Write(ssep + "1");
+                        }
+                        else{
+                            writer.Write(ssep + "0");
+                        }                        
+                    }
+                }
+                writer.WriteLine();
+            }
+            writer.Close();
+
+
+        }
+
+        private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Settings.Default.Filename = FilenameControl.Text;
+            Settings.Default.Save();
+        }
+
+        private void ProcessButton_Click(object sender, EventArgs e)
+        {
+            //init
+            int outputIndex = OutputStartIndexControl.Value;
+            string filename = FilenameControl.Text;
+            int binCount = (int)BinCountControl.Value;
+            char csep;
+            if (SeparatorControl.Text == "\\t")
+            {
+                csep = '\t';
+            }
+            else
+            {
+                csep = SeparatorControl.Text[0];
+            }
+            List<int> NumericFields = null;
+            if (NumericFieldsControl.Text.Length > 0)
+            {
+                NumericFields = NumericFieldsControl.Text.Split(',').Select(el => int.Parse(el.Trim())).ToList();
+            }
+
+            //read
+            StreamReader reader = new StreamReader(filename);
+            var header = reader.ReadLine();
+            var headers = header.Split(csep).ToList();
+            List<List<String>> lines = new List<List<String>>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var fields = line.Split(csep).ToList();
+                lines.Add(fields);
+            }
+            reader.Close();
+
+            //identify categories
+            var sample = lines[0];
+            List<int> indexes = new List<int>();
+            for (int i = 0; i < sample.Count; i++)
+            {
+                Decimal dummy;
+                if (!Decimal.TryParse(sample[i], out dummy))
+                {
+                    indexes.Add(i);
+                }
+            }
+            indexes.RemoveAll(el => NumericFields.Contains(el));
+
+            //get categories
+            List<List<String>> dimensions = new List<List<String>>();
+            foreach (var index in indexes)
+            {
+                dimensions.Add(lines.Select(line => line[index]).Distinct().ToList());
+            }
+
+            //get maxes
+            List<decimal> maxes = new List<decimal>();
+            foreach (var index in NumericFields)
+            {
+
+                maxes.Add(lines.Select(el => el[index]).Where(el => el != "").Select(el => Decimal.Parse(el)).Max());
+            }
+
+            //get mins
+            List<decimal> mins = new List<decimal>();
+            foreach (var index in NumericFields)
+            {
+                mins.Add(lines.Select(el => el[index]).Where(el => el != "").Select(el => Decimal.Parse(el)).Min());
+            }
+
+            //set step
+            List<decimal> steps = new List<decimal>();
+            for (int i = 0; i < NumericFields.Count; i++)
+            {
+                steps.Add((maxes[i] - mins[i]) / binCount);
+            }
+
+            //write input header            
+            StreamWriter writer = new StreamWriter(Path.ChangeExtension(filename, " - input.sparsed.tsv"));
+            for (int j = 0; j < headers.Count; j++)
+            {
+                if (j >= outputIndex) {
+                    break;
+                }
+                var val = headers[j];
+                if (indexes.Contains(j))
+                {
+                    int jx = indexes.IndexOf(j);
+                    foreach (var dim in dimensions[jx])
+                    {
+                        var s = val + " - " + dim + csep;
+                        Console.WriteLine(s);
+                        writer.Write(s);
+                    }
+                }
+                else if (NumericFields.Contains(j))
+                {
+                    for (var i = 0; i < binCount; i++)
+                    {
+                        var s = val + " - " + i.ToString() + csep;
+                        Console.WriteLine(s);
+                        writer.Write(s);
+                    }
+                    String ss = val + " - " + "Não Definido" + csep;
+                    Console.WriteLine(ss);
+                    writer.Write(ss);
+                }
+                else
+                {
+                    String ss = val + csep;
+                    Console.WriteLine(ss);
+                    writer.Write(ss);
+                }
+                
+            }
+            writer.WriteLine();
+
+            //write input content
+            for (int i = 0; i < lines.Count; i++)
+            {
+                var line = lines[i];
+                for (int j = 0; j < line.Count; j++)
+                {
+                    if (j >= outputIndex)                    
+                        break;
+                    
+                    var val = line[j];
+
+                    //for categories
+                    if (indexes.Contains(j))
+                    {
+                        var jx = indexes.IndexOf(j);
+                        foreach (var dim in dimensions[jx])
+                        {
+                            if (dim == val)
+                            {
+                                writer.Write("1" + csep);
+                            }
+                            else
+                            {
+                                writer.Write("0" + csep);
+                            }
+                        }
+                    }
+
+                    //for discretized
+                    else if (NumericFields.Contains(j))
+                    {
+                        var jx = NumericFields.IndexOf(j);
+                        int bin;
+                        if (val == "")
+                        {
+                            bin = binCount;
+                        }
+                        else
+                        {
+                            var nval = Decimal.Parse(val);
+                            bin = (int)Decimal.Truncate(((nval - mins[jx] - steps[jx] / 10000000) / steps[jx]));
+                        }
+                        for (int k = 0; k < binCount - 1; k++)
+                        {
+                            if (k == bin)
+                            {
+                                writer.Write("1" + csep);
+                            }
+                            else
+                            {
+                                writer.Write("0" + csep);
+                            }
+                        }
+                        if (bin == binCount)
+                            writer.Write("1" + csep);
+                        else
+                            writer.Write("0" + csep);
+                    }
+                    else
+                        writer.Write(val + csep);
+                }
+                writer.WriteLine();
+            }
+            writer.Close();
+
+            //write output header
+            if (outputIndex > -1)
+            {
+                writer = new StreamWriter(Path.ChangeExtension(filename, " - output.sparsed.tsv"));
+                for (int j = outputIndex; j < headers.Count; j++)
+                {
+
+                }
+            }
+        }
+      
+    }
+}
